376234ec4f9ab1fd7b1bbf261f472fdd40a47117
EshaSajid <92621657+EshaSajid@users.noreply.github.com>
2022-01-06 18:47:28 +0500

    Use verification and Home page done

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,7 @@ export { NavigationBar } from './NavigationBar';
 export { DashboardSideBar } from './DashboardSideBar';
 export { DashboardContainer } from './DashboardContainer';
 export { DashboardSideBarItem } from './DashboardSideBarItem';
+export { Header } from './Header';
+export { Destination } from './Destination';
+export { Error } from './Error';
+export { Showcase } from './Showcase';
